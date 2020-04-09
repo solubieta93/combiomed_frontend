92fda61c6e406f7160b693de2bf8836b98700299
Oscar <o.martin9203@gmail.com>
2020-04-09 05:41:24 -0400

    generics components

--- src/views/products/Products.vue
+++ src/views/products/Products.vue
@@ -13,79 +13,27 @@
       :src="`${baseUrl}web-combiomed-historia-03.png`"
       style="top:-46px"
     />
-    <v-row
-      justify="center"
-      align="center"
+    <paginate-items
+      :can-change-limit="true"
+      :default-limit="10"
+      :filter-items="filterItems"
+      :show-divider="true"
+      :show-limit="true"
+      :show-search-text="true"
+      :update-on-change-text="true"
     >
-      <v-row
-        justify="center"
-        align="center"
-        style="max-width: 50vw"
-      >
-        <v-col class="mr-0">
-          <query-search
-            :loading="loading"
-            @search:text="appliedSearch"
-            :onKeyUp="true"
-          ></query-search>
-        </v-col>
-        <v-col>
-          <v-row
-            justify="start"
-            align="start"
-            class="ml-0"
-          >
-            <v-text-field
-              v-model="limit"
-              class="mx-auto"
-              type="number"
-              :min="0"
-              :max="count"
-              :disabled="!count"
-              label="Cantidad"
-              style="width: 50px; max-width: 50px"
-            />
-          </v-row>
-        </v-col>
-      </v-row>
-    </v-row>
-    <v-row
-      align="center"
-      justify="center"
-    >
-      <v-col class="px-6">
-        <v-divider />
-      </v-col>
-    </v-row>
-    <v-row
-      justify="center"
-      align="center"
-    >
-      <v-row
-        justify="center"
-        align="center"
-        style="max-width: 80vw"
-      >
-        <v-col
-          v-for="(item, i) in productsItems"
-          :key="i"
-        >
-          <item-preview
-            :item="item.item"
-            :pathTo="item.pathTo"
-          ></item-preview>
-        </v-col>
-      </v-row>
-    </v-row>
+    </paginate-items>
   </v-container>
 </template>
 <script>
-  import ItemPreview from '@/components/core/ItemPreview'
-  import QuerySearch from '@/components/core/QuerySearch'
+  // import ItemPreview from '@/components/core/ItemPreview'
+  // import QuerySearch from '@/components/core/QuerySearch'
+  import PaginateItems from '../../components/core/PaginateItems'
   export default {
     components: {
-      ItemPreview,
-      QuerySearch,
+      // ItemPreview,
+      // QuerySearch,
+      PaginateItems,
     },
     data () {
       return {
@@ -101,15 +49,6 @@
         text: '',
       }
     },
-    async mounted () {
-      await this.filterProducts()
-    },
-    watch: {
-      async limit (value) {
-        this.page = 1
-        await this.filterProducts()
-      },
-    },
     computed: {
       countPages () {
         return this.count / this.limit
@@ -120,6 +59,15 @@
       productsItems () {
         return this.products.map(x => this.buildItem(x))
       },
+    },
+    watch: {
+      async limit (value) {
+        this.page = 1
+        await this.filterProducts()
+      },
+    },
+    async mounted () {
+      await this.filterProducts()
     },
     methods: {
       async appliedSearch (text) {
@@ -150,6 +98,22 @@
             owner: product.owner,
           },
           pathTo: `/products/${product.id}`,
+        }
+      },
+      async filterItems (search, offset, limit) {
+        console.log(search, 'text')
+        this.loading = true
+        const result = await this.$store.dispatch('getProducts', {
+          offset, limit, search,
+        })
+        console.log(result, 'filterProducts')
+        const { products, count } = result
+        this.products = products
+        this.count = count
+        this.loading = false
+        return {
+          items: this.products.map(x => this.buildItem(x)),
+          count,
         }
       },
     },
