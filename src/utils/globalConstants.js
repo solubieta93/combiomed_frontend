a0635984d35f1d2f82466c66ae9facbf13e0c9e8
Oscar <o.martin9203@gmail.com>
2020-04-16 13:25:18 -0400

    update uri of backend

--- src/utils/globalConstants.js
+++ src/utils/globalConstants.js
@@ -1,4 +1,4 @@
-export const apiURI = process.env.NODE_ENV === 'production' ? 'https://apinest.anandaspa.us' : 'http://localhost:8000'
+export const apiURI = process.env.NODE_ENV === 'production' ? 'https://api-combiomed.herokuapp.com' : 'https://api-combiomed.herokuapp.com'
 export const apiGqlEndpoint = apiURI + 'graphql'
 export const api = {
     uri: apiURI,
