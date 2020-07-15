51de684bd4225e7e0ef49dcf43a862650ef8f8ea
Sol <solubieta93@gmail.com>
2020-07-15 18:57:31 -0400

    global constants

--- src/utils/globalConstants.js
+++ src/utils/globalConstants.js
@@ -1,3 +1,9 @@
+// TODO: COMMENT TO USE IN DEPLOY
 export const apiURI = process.env.NODE_ENV === 'production' ? 'https://api-combiomed.herokuapp.com' : 'http://127.0.0.1:8000'
-export const baseUrl = process.env.NODE_ENV === 'production' ? 'https://combiomed.biocubafarma.cu/' : 'http://127.0.0.1:8080/'
+export const baseUrl = process.env.NODE_ENV === 'production' ? 'https://combiomed.herokuapp.com/' : 'http://127.0.0.1:8080/'
+
+// TODO: UNCOMMENT TO USE IN DEPLOY
+// export const apiURI = process.env.NODE_ENV === 'production' ? 'https://api-combiomed.biocubafarma.com' : 'http://127.0.0.1:8000'
+// export const baseUrl = process.env.NODE_ENV === 'production' ? 'https://combiomed.biocubafarma.com/' : 'https://combiomed.biocubafarma.com/'
+
 export const apiKeyFileStack = 'AvM4GawwuTFeUBzWR3yX9z'
