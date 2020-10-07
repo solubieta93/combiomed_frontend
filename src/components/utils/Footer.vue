5a1b4b5e0904dd5b2a4a0a285250cf7d3cde4d06
Sol <solubieta93@gmail.com>
2020-10-07 16:01:18 -0400

    adding social network route

--- src/components/utils/Footer.vue
+++ src/components/utils/Footer.vue
@@ -12,15 +12,22 @@
         <v-row justify="center">
           <v-list-item-icon
             v-for="i in icons"
-            :key="i"
+            :key="i.icon"
             style="margin-left:3px; margin-right: 3px;"
           >
-            <v-icon
-              color="grey"
-              class="ma-0"
+            <v-btn
+              icon
+              small
+              :href="i.route"
             >
-              {{ i }}
-            </v-icon>
+              <v-icon
+                color="grey"
+                class="ma-0"
+              >
+                {{ i.icon }}
+              </v-icon>
+            </v-btn>
+
           </v-list-item-icon>
         </v-row>
       </v-col>
@@ -57,9 +64,10 @@
       return {
         baseUrl: process.env.BASE_URL,
         icons: [
-          'fab fa-facebook',
-          'fab fa-twitter',
-          'fab fa-google-plus',
+          { icon: 'fab fa-facebook', route: 'https://www.facebook.com/COMBIOMED-2269852883332348/' },
+          { icon: 'fab fa-twitter', route: 'https://mobile.twitter.com/combiomedc' },
+          { icon: 'fab fa-youtube', route: 'https://www.youtube.com/channel/UCojTT9zhJblDjNvVZiTpsTQ' },
+          // 'fab fa-google-plus',
           // 'fab fa-linkedin',
           // 'fab fa-instagram',
         ],
